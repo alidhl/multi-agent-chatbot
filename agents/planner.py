e20d4d12eb7aae22b0b88fbe63db988aebe87e66
alidhl <119793124+alidhl@users.noreply.github.com>
2024-03-21 00:10:11 +0300

    optimized prompt

--- agents/planner.py
+++ agents/planner.py
@@ -13,11 +13,11 @@ class Planner(BaseModel):
 
 def get_planner():
     planner_prompt = ChatPromptTemplate.from_template(
-        """For the given objective, come up with a simple and concise step by step plan. \
-    This plan should involve individual tasks, that if executed correctly will yield the correct answer. Do not add any superfluous steps. \
-    The result of the final step should be the final answer. Make sure that each step has all the information needed - do not skip steps.
+"""Given the objective, devise a simple and concise step-by-step plan that involves using a search engine to find the answer. \
+This plan should consist of individual search queries that, if executed correctly, will yield the correct answer. \
+Avoid unnecessary steps and aim to make the plan as short as possible. The result of the final step should be the final answer. \
 
-    {objective}"""
+{objective}"""
     )
     return create_structured_output_runnable(
         output_schema= Planner,
@@ -37,18 +37,18 @@ class Response(BaseModel):
 
 def get_replanner():
     replanner_prompt = ChatPromptTemplate.from_template(
-    """You are a replanner agent. Your role is to update the execution plan of another agent to achieve a given objective, step by step. If the answer to the objective is already found in the past steps, your task is to return that answer.\
+    """Your task is to revise the current plan based on the executed steps. Remove any steps that have been completed and ensure the remaining steps will lead to the complete answer for the objective. Remember, the objective should be fully answered, not just partially. If the answer is already found in the executed steps, return the answer to the objective.
 
-    Here is your objective:
-    {input}
+Objective:
+{input}
 
-    Here is your current plan:
-    {plan}
+Current Plan:
+{plan}
 
-    These are the steps that have been executed so far:
-    {past_steps}
+Executed Steps:
+{past_steps}
 
-    Based on the execution of the plan so far, your task is to update the plan by removing the completed steps. Ensure that the remaining steps in the plan will lead to the answer for the objective. Remember, make sure to answer the objective fully not just part of it."""
+"""
     )
     return create_openai_fn_runnable(
         functions= [Planner, Response],
@@ -59,13 +59,11 @@ def get_replanner():
 
 # Testing
 if __name__ == "__main__":
-    query = "When was Saudi Arabia The Line project announced?"
+    query = "How do I get a passport in saudi arabia"
     planner = get_planner()
     replanner = get_replanner()
     
     print(query + "\n\n")
-    print(planner.invoke({"objective" : query}))
-    print("-"*50)
     print(planner.invoke({"objective" : query}).steps)
     
     
